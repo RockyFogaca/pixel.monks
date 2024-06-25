db601db8f1e719f23a6499da7c31a27bf41d9ffe
Gabriel Fogaça Silva <gabriel.silva@mediamonks.com>
2024-06-25 17:52:32 -0300

    Remoção do github actions

---
+++ gf.js
@@ -0,0 +1,17 @@
+function gf(eventObjectLocal) {
+
+    const eventData = eventObjectLocal;
+    eventData.items = eventObjectLocal.items; // editar com filtros necessarisos
+
+    // Colocar regras e tratamentos
+
+
+    //console.log("-----")
+    //console.log(eventObjectLocal)
+    //console.log("-----")
+    //console.log("-----")
+    //console.log(eventData)
+    //console.log("-----")
+
+    APIMM(eventData);
+}
